2bfb7764b7e5f4e1fb64ff807578b461973124f1
Doug Vargas <dougvargas72@gmail.com>
2026-05-27 10:40:49 -0500

    feat(v1.13.14): fecha+hora 2 líneas en director/bandeja y coordinador/actos

--- prototype/shared/naowee-footer.js
+++ prototype/shared/naowee-footer.js
@@ -15,7 +15,7 @@
    ═══════════════════════════════════════════════════════════════════════ */
 
 (function mountNaoweeFooter() {
-  var IVC_VERSION = 'v1.13.13';
+  var IVC_VERSION = 'v1.13.14';
   var REPO = 'naowee-tech/naowee-ivc';
   var MODULE_NAME = 'IVC';
 
