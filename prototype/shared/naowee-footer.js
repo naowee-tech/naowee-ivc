6c19e59879d46d767c298c05292adec9abb7fc99
Doug Vargas <dougvargas72@gmail.com>
2026-05-27 10:42:58 -0500

    feat(v1.13.15): btn-firmar con glow verde hover + remove Cancelar redundante

--- prototype/shared/naowee-footer.js
+++ prototype/shared/naowee-footer.js
@@ -15,7 +15,7 @@
    ═══════════════════════════════════════════════════════════════════════ */
 
 (function mountNaoweeFooter() {
-  var IVC_VERSION = 'v1.13.14';
+  var IVC_VERSION = 'v1.13.15';
   var REPO = 'naowee-tech/naowee-ivc';
   var MODULE_NAME = 'IVC';
 
