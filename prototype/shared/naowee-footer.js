edfdb7abe8a91e420079487b7de2342aedc742b3
Doug Vargas <dougvargas72@gmail.com>
2026-05-27 10:05:55 -0500

    feat(v1.13.10): Datos tab titles gris + Historial pattern colapsable global

--- prototype/shared/naowee-footer.js
+++ prototype/shared/naowee-footer.js
@@ -15,7 +15,7 @@
    ═══════════════════════════════════════════════════════════════════════ */
 
 (function mountNaoweeFooter() {
-  var IVC_VERSION = 'v1.13.9';
+  var IVC_VERSION = 'v1.13.10';
   var REPO = 'naowee-tech/naowee-ivc';
   var MODULE_NAME = 'IVC';
 
